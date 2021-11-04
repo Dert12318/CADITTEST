c79536acf882e6391cfcbb32050dc79e05ceba1d
Dert12318 <devanrt140999@gmail.com>
2021-11-04 20:13:06 +0700

    soal 1-1

--- usecase/Soal1/Soal1.go
+++ usecase/Soal1/Soal1.go
@@ -5,21 +5,25 @@ import (
 	"CadItTest/models"
 	"CadItTest/repository"
 	"CadItTest/usecase"
+
+	"github.com/gin-gonic/gin"
 )
+
 var context *gin.Context
+
 type UsecaseSoal1Struct struct {
 	testRepo repository.TestRepoInterface
 	log      *log.LogCustom
 }
 
-func NewUsecaseImpl(TestRepo repository.TestRepoInterface, log *log.LogCustom) usecase.SoalUsecaseInterface {
-	return &UsecaseSoal1Struct{TestRepo, log}
+func NewUsecaseImpl(testRepo repository.TestRepoInterface, log *log.LogCustom) usecase.SoalUsecaseInterface {
+	return &UsecaseSoal1Struct{testRepo, log}
 }
 
 func (b UsecaseSoal1Struct) Soal1Func(input models.DataRequest, id, traceHeader map[string]string) (models.DataRequest, error) {
 	url := "http://jsonplaceholder.typicode.com/users"
-	if (id==nil){
-		b.GetDataAllPartner(url, context, traceHeader,)
+	if id == nil {
+		b.GetDataAllPartner(url, context, traceHeader)
 	}
 	return input, nil
 }
